9c799ff8fe30c8e94020bc45f361c12a49fbede0
gavhari <gaberingo12@gmail.com>
2021-05-22 16:58:30 +0700

    update listbuku dan database

--- app/Http/Controllers/listBuku.php
+++ app/Http/Controllers/listBuku.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class listBuku extends Controller
 {
     //
     function list_buku(){
-        return view('listbuku');
+        $list_buku = DB::table('list_buku')->get();
+        dump($list_buku);
+        return view('listbuku',['daftar_buku' => $list_buku]);
     }
 }
--- resources/views/listbuku.blade.php
+++ resources/views/listbuku.blade.php
@@ -1,27 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
-    <title>Document</title>
-</head>
-<body>
-    <div class="list-group">
-        <button type="button" class="list-group-item list-group-item-action">
-            Cras justo odio
-        </button>
-        <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
-        <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
-        <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
-        <button type="button" class="list-group-item list-group-item-action">Vestibulum at eros</button>
-    </div>
-
-    <!--Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
-</body>
-</html>
+@extends('layouts.master')
+@section('content')
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Judul Buku</th>
+        <th scope="col">Pengarang</th>
+        <th scope="col">Penerbit</th>
+        <th scope="col">Tahun Terbit</th>
+        <th scope="col">Tebal Halaman</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($daftar_buku as $buku)
+        <tr>
+            <th scope="row">{{$buku->id}}</th>
+            <td>{{$buku->judul_buku}}</td>
+            <td>{{$buku->pengarang}}</td>
+            <td>{{$buku->penerbit}}</td>
+            <td>{{$buku->tahun_terbit}}</td>
+            <td>{{$buku->tebal_halaman}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+@endsection
