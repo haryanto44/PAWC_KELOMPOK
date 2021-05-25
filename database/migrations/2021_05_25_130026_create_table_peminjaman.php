29269d5638ba80fe44737a388127f82a44b68bcf
gnav404 <59043571+gnav404@users.noreply.github.com>
2021-05-25 20:13:27 +0700

    list + peminjaman

---
+++ database/migrations/2021_05_25_130026_create_table_peminjaman.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePeminjaman extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('buku');
+			$table->string('user');
+			$table->timestamps();
+			
+			$table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('peminjaman', function (Blueprint $table) {
+            //
+        });
+    }
+}
